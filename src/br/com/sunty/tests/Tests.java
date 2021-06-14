e1abef59bcc2f2bea40a259c8fcd986508d73f40
Fábio Pereira <fabiuhp@msn.com>
2021-06-14 11:09:36 -0300

    Apos curso com Nico, refatorando o codigo tirando o toString

--- src/br/com/sunty/tests/Tests.java
+++ src/br/com/sunty/tests/Tests.java
@@ -22,30 +22,30 @@ public class Tests {
         //Criar vídeo
         Video video1 = new Video(1L, "Video inicial Java", "inicio-java", section1, "video-java");
 
-        System.out.println(video1.toString());
+        System.out.println(video1);
         video1.setTime(2);
         video1.setTranscription("Um video inicial sobre java");
 
-        System.out.println(video1.toString());
+        System.out.println(video1);
 
         //Criar questão
         Question question1 = new Question(1L, "O que significa POO?", "questao-poo", section1, "Pergunta 1", QuestionType.MULTIPLEANSWER);
-        System.out.println(question1.toString());
+        System.out.println(question1);
 
         //Criar explicação
         Explanation explanation1 = new Explanation(1L, "Explic 1", "explic", section1, "Texto da explicação.");
-        System.out.println(explanation1.toString());
+        System.out.println(explanation1);
 
         //Criar alternativa
         Alternative alternative1 = new Alternative(1L, "Explicacao 1 da alternativa", false, question1);
         Alternative alternative2 = new Alternative(2L, "Explicacao 2 da alternativa", true, question1);
-        System.out.println(alternative1.toString());
-        System.out.println(alternative2.toString());
+        System.out.println(alternative1);
+        System.out.println(alternative2);
 
         //Criar subcategoria
         SubCategory subCategory1 = new SubCategory(1L, "SubCategoria 1", "subcat-cde", category1);
         SubCategory subCategory2 = new SubCategory(2L, "SubCategoria 2", "subcat-abc", category1);
-        System.out.println(subCategory1.toString());
-        System.out.println(subCategory2.toString());
+        System.out.println(subCategory1);
+        System.out.println(subCategory2);
     }
 }
