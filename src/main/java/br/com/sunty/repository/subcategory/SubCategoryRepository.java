38c9bf8139d8c6ac6e5f327016b2cee01026570d
Fabio Pereira <fabiuhp@msn.com>
2021-08-23 14:14:33 -0300

    assertj e CategoryRepositoryTest

--- src/main/java/br/com/sunty/repository/subcategory/SubCategoryRepository.java
+++ src/main/java/br/com/sunty/repository/subcategory/SubCategoryRepository.java
@@ -1,6 +1,5 @@
 package br.com.sunty.repository.subcategory;
 
-import br.com.sunty.models.category.Category;
 import br.com.sunty.models.category.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,6 +8,5 @@ import java.util.Optional;
 
 public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
     Optional<SubCategory> findByUrlCode(String urlCode);
-    Optional<SubCategory> findAllByCategoryOrderByOrderToShowAsc(Category category);
     List<SubCategory> findAllByCategoryUrlCode(String categoryUrlCode);
 }
---
+++ src/test/java/br/com/sunty/repository/category/CategoryRepositoryTest.java
@@ -0,0 +1,80 @@
+package br.com.sunty.repository.category;
+
+import br.com.sunty.models.category.Category;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void shouldFindByUrlCode() {
+        String categoryName = "devops";
+        Category category = categoryRepository.findByUrlCode(categoryName)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        assertThat(category.getName())
+                .isEqualTo(categoryName);
+    }
+
+    @Test
+    void shouldFindAllByActive() {
+        List<Category> categories = categoryRepository.findAllByActive(true);
+
+        assertThat(categories)
+                .hasSize(3)
+                .extracting(Category::getUrlCode)
+                .contains("devops", "programacao", "front-end");
+    }
+
+    @Test
+    void shouldFindAllByOrderByName() {
+        List<Category> categories = categoryRepository.findAllByOrderByName();
+
+        assertThat(categories)
+                .hasSize(4)
+                .extracting(Category::getUrlCode)
+                .containsSequence("business", "devops", "front-end", "programacao");
+    }
+
+    @Test
+    void shouldFindActiveCategoriesWithActiveSubCategoriesAndPublicCourses() {
+        List<Category> categories = categoryRepository.findActiveCategoriesWithActiveSubCategoriesAndPublicCourses();
+
+        assertThat(categories)
+                .hasSize(3)
+                .extracting(Category::getUrlCode)
+                .contains("front-end", "devops", "programacao");
+    }
+
+    @Test
+    void shouldFindCategoryActiveByUrlCode() {
+        String activeCategory = "programacao";
+        Category category = categoryRepository.findCategoryActiveByUrlCode(activeCategory)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        assertThat(category).isNotNull();
+    }
+
+    @Test
+    void shouldNotFindCategoryActiveByUrlCode() {
+        String inactiveCategory = "business";
+
+        assertThatThrownBy(() -> categoryRepository.findCategoryActiveByUrlCode(inactiveCategory)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)))
+                .isInstanceOf(ResponseStatusException.class);
+    }
+}
