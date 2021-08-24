6e30694be9c867919decaf49676f3b5f2e645130
Fabio Pereira <fabiuhp@msn.com>
2021-08-24 12:55:49 -0300

    teste de controller

--- src/test/java/br/com/sunty/controller/category/api/CategoryApiControllerTest.java
+++ src/test/java/br/com/sunty/controller/category/api/CategoryApiControllerTest.java
@@ -1,14 +1,22 @@
 package br.com.sunty.controller.category.api;
 
+import br.com.sunty.models.category.Category;
+import br.com.sunty.repository.category.CategoryBuilder;
+import br.com.sunty.repository.category.CategoryRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import javax.transaction.Transactional;
 import java.net.URI;
-import java.net.URISyntaxException;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,14 +26,31 @@ class CategoryApiControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Test
-    void shouldFindAllActiveCategories() throws URISyntaxException {
+    @Transactional
+    void shouldFindAllActiveCategories() throws Exception {
+        Category category = new CategoryBuilder("programacao", "programacao").build();
+        categoryRepository.save(category);
+
         URI uri = new URI("/api/categories");
 
-//        mockMvc.perform()
+        mockMvc.perform(MockMvcRequestBuilders
+                .get(uri)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
     @Test
-    void clearCache() {
+    void clearCache() throws Exception {
+        URI uri = new URI("/api/bGltcGEtby1jYWNoZS1kYS1hcGktYWU");
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get(uri)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
